e0e74a32309e9f6381b6e7336a5a4608ed1dc3d5
shayan <shayan@umd.edu>
2022-02-15 13:16:35 -0500

    updated requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import pathlib
 from setuptools import setup
-from logsnag import __version__
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
     name="logsnag",
-    version=__version__,
+    version='0.0.1',
     description="LogSnag API Wrapper",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -15,7 +14,15 @@ setup(
     author="Shayan Taslim",
     author_email="shayan@logsnag.com",
     license="MIT",
+    python_requires='>=3.6',
     packages=["logsnag"],
     include_package_data=True,
     install_requires=["requests"],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
 )
