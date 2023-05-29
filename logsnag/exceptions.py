26dde4aca04f16c8f28bc1d2b2a259ce79550cf2
Daniel S. Billing <daniel@izy.no>
2023-05-29 22:37:05 +0200

    Improved exception handling for easier debugging

--- logsnag/exceptions.py
+++ logsnag/exceptions.py
@@ -3,3 +3,10 @@
 
 class FailedToPublish(Exception):
     """Failed To Publish Exception"""
+
+    def __init__(self, msg='', data=None):
+        self.msg = msg
+        self.data = data
+
+    def __str__(self):
+        return f'{self.msg}: {self.data}'
