c8302c510c22333663741f3882993026bf0fa94e
Shayan <ltaslim@montgomerycollege.edu>
2023-08-31 14:35:54 -0400

    v1.0.1

--- logsnag/version.py
+++ logsnag/version.py
@@ -1,3 +1,3 @@
 """Package Version"""
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="logsnag",
-    version='1.0.0',
+    version='1.0.1',
     description="LogSnag API Wrapper",
     long_description=README,
     long_description_content_type="text/markdown",
